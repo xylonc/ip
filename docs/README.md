cc2aa24d6ee84d039be41eb3e463a1e14c4f93a4
Xylon <xylonchan229@gmail.com>
2025-08-28 17:20:11 +0800

    Renaming duke chatbot name

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Victoria User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Victoria.java
@@ -0,0 +1,19 @@
+public class Victoria {
+    public static void main(String[] args) {
+        String logo =
+                " __     ___  ____ \n"
+                        + " \\ \\   / / |/ ___|\n"
+                        + "  \\ \\ / /| ' /    \n"
+                        + "   \\ V / | . \\___ \n"
+                        + "    \\_/  |_|| ___|  \n";
+
+        System.out.println("Hello from\n" + logo);
+        System.out.println("-".repeat(30));
+        System.out.println("Hello! I'm Victoria");
+        System.out.println("How may I help you?\n");
+        System.out.println("-".repeat(30));
+        System.out.println("Bye. Hope to see you soon!\n");
+        System.out.println("-".repeat(30));
+
+    }
+}
