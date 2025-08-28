237addeb395608fc9a759ca4e7038266b3206d82
Xylon <xylonchan229@gmail.com>
2025-08-28 23:36:06 +0800

    Adding the echo method

--- src/main/java/Victoria.java
+++ src/main/java/Victoria.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Victoria {
     public static void main(String[] args) {
         String logo =
@@ -11,8 +13,19 @@ public class Victoria {
         System.out.println("-".repeat(30));
         System.out.println("Hello! I'm Victoria");
         System.out.println("How may I help you?\n");
+        System.out.println("-".repeat(30) + "\n");
+        String answer;
+        do {
+            Scanner reply = new Scanner(System.in);
+            answer = reply.nextLine();
+            System.out.println("-".repeat(30));
+            System.out.println(answer + "\n");
+            System.out.println("-".repeat(30) + "\n");
+        }
+        while(!answer.equals("bye"));
+
         System.out.println("-".repeat(30));
-        System.out.println("Bye. Hope to see you soon!\n");
+        System.out.println("Bye.Hope to see you again soon!\n");
         System.out.println("-".repeat(30));
 
     }
